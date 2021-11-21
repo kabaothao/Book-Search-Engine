bcddd2c67ee143dee4ce84f76258b4665b6b441a
Kabao Thao <tkabao@gmail.com>
2021-11-21 13:53:26 -0600

    update code in server file

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//TODO: set up server with ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: 
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

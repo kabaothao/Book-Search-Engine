a0bb324fea638dc1ae8aea1c41ba490f68250725
Kabao Thao <tkabao@gmail.com>
2021-11-29 15:42:34 -0600

    update server.js and package json

--- server/server.js
+++ server/server.js
@@ -19,17 +19,17 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-// //changing REST APIs to graphQL
-// // app.use(routes);
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-// // console.log("Test Message 2", db._connectionString);
+//changing REST APIs to graphQL
+// app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+// console.log("Test Message 2", db._connectionString);
 
 db.once("open", () => {
   app.listen(PORT, () => {
